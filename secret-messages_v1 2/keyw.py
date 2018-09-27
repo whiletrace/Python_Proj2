d480d44a6027b152ef5d02e6ed3fd7c5c4c4b417
Trace Harris <traceh@whiletrace.com>
2018-09-26 21:48:18 -0700

    All Cipher classes encrypt and decrypt methods outputting as expected next to implement a menu and make sure all is under pep8 standards

--- secret-messages_v1 2/keyw.py
+++ secret-messages_v1 2/keyw.py
@@ -3,12 +3,13 @@ import string
 from ciphers import Cipher
 
 class Keyw(Cipher):
-    """docstring for Keyword"""
+    """docstring for Keyw"""
     def __init__(self):
         super (Keyw, self).__init__()
         self.alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M',
                       'N','O','P','Q','R','S','T','U','V','W','X', 'Y','Z']
         
+
 
     def scrub_duplicates(self, list):
         ciphertext= [] 
@@ -20,7 +21,7 @@ class Keyw(Cipher):
     
 
     def encrypt(self, text, keyword):
-        output = []
+        
         key = list(keyword)
 
         X = [key.append(char) for char in self.alpha]
@@ -30,7 +31,24 @@ class Keyw(Cipher):
         cipher = self.scrub_duplicates(key)
 
         ciphert =[cipher[i] for i in chartoi]
+
         print(''.join(ciphert))
 
+    
+
     def decrypt(self, text, keyword):
-        pass
+
+        key = list(keyword)
+
+        X = [key.append(char) for char in self.alpha]
+
+        cipher = self.scrub_duplicates(key)
+
+        cipherdict = dict(zip(cipher, list(range(26))))
+
+        ciphertoi =[cipherdict[i] for i in text]
+
+        plaintext =[self.inttochar(i) for i in ciphertoi]
+
+        print(''.join(plaintext))
+
